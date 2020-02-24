d2eaa48878b2d3b6a5d3699654dde0c5234b711f
jonnyyu <yingshen.yu@gmail.com>
2020-02-24 18:08:20 +0800

    add auto rename for dealing invalid field names

--- src/json2obj/__init__.py
+++ src/json2obj/__init__.py
@@ -14,5 +14,5 @@ finally:
 import json
 from collections import namedtuple
 
-def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values(), rename=True)
 def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
